3e735a1ecb145aa748c933d66e79772b6a4b2db8
Jack Phillips <jack.phillips.mail@gmail.com>
2014-02-20 14:16:03 -0500

    Added BAC calculator
    Updates to files:
    - AndroidManifest.xml
    - activity_main.xml
    - R.java
    - MainActivity.java
    - activity_bac.xml

--- DrinkMate/gen/com/isat480/drinkmate/R.java
+++ DrinkMate/gen/com/isat480/drinkmate/R.java
@@ -5,14 +5,23 @@
  * should not be modified by hand.
  */
 
-package com.isat480.drinkmate;
+package com.isat.drinkmate;
 
 public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int pearl=0x7f060001;
-        public static final int slate=0x7f060000;
+        public static final int background=0x7f040002;
+        public static final int baseblue=0x7f04000a;
+        public static final int black=0x7f040009;
+        public static final int bluedark=0x7f040004;
+        public static final int bluelight=0x7f040003;
+        public static final int greendark=0x7f040006;
+        public static final int greenlight=0x7f040005;
+        public static final int grey=0x7f040001;
+        public static final int orangedark=0x7f040008;
+        public static final int orangelight=0x7f040007;
+        public static final int white=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,28 +30,49 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int btnButton=0x7f090001;
-        public static final int ivWrench=0x7f090002;
-        public static final int strTest=0x7f090000;
+        public static final int action_settings=0x7f09000d;
+        public static final int btnBac=0x7f09000c;
+        public static final int btnCalc=0x7f090001;
+        public static final int btnMain=0x7f09000b;
+        public static final int gender=0x7f090002;
+        public static final int genderEt=0x7f090008;
+        public static final int hours=0x7f090005;
+        public static final int numDrinks=0x7f090004;
+        public static final int numDrinksEt=0x7f090006;
+        public static final int resultTv=0x7f090009;
+        public static final int textView2=0x7f090000;
+        public static final int timeEt=0x7f09000a;
+        public static final int weight=0x7f090003;
+        public static final int weightEt=0x7f090007;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_bac=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int bac=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int strTest=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int bac_button=0x7f060003;
+        public static final int calculate_button=0x7f060005;
+        public static final int gender_lbl=0x7f060006;
+        public static final int height_lbl=0x7f060007;
+        public static final int hello_world=0x7f06000b;
+        public static final int hours_lbl=0x7f06000a;
+        public static final int num_drinks_lbl=0x7f060009;
+        public static final int title_activity_bac=0x7f060004;
+        public static final int weight_lbl=0x7f060008;
+        public static final int welcome_message=0x7f060002;
     }
     public static final class style {
         /** 
--- DrinkMate/src/com/isat480/drinkmate/MainActivity.java
+++ DrinkMate/src/com/isat480/drinkmate/MainActivity.java
@@ -2,45 +2,31 @@ package com.isat480.drinkmate;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	Button btnTest;
-	TextView tvTest;
-	ImageView imgView;
 	
+	public final static String EXTRA_MESSAGE = "INTENT FROM MAIN";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		addListenerOnButton();
 	}
-
+		
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	public void addListenerOnButton() {
-		btnTest = (Button) findViewById(R.id.btnButton);
-		btnTest.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tvTest = (TextView) findViewById(R.id.strTest);
-				tvTest.setText("   Welcome to DrinkMate!\nWe're still working on things");
-				imgView = (ImageView) findViewById(R.id.ivWrench);
-				imgView.setVisibility(1);
-			}
-			
-		});
+	public void startBac(View view)
+	{
+		// changes the view to BAC Calculator
+		Intent intent = new Intent(this, BacActivity.class);
+		startActivity(intent);
+		
 	}
 }
